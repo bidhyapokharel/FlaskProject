aada143ec77e671fab18d5bdc7ba584f324fdf02
Bidhya Pokharel <bidhyapokharel1@gmail.com>
2021-05-09 19:45:36 +0545

    Create Registration form

--- FlaskProject/market/forms.py
+++ FlaskProject/market/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 
 class RegisterForm(FlaskForm):
-    username = StringField(label='username')
-    email_address = StringField(label='email_address')
+    username = StringField(label='Username: ')
+    email_address = StringField(label='Email Address: ')
     password1 = PasswordField(label='Password')
     password2 = PasswordField(label='Confirm Password')
     submit = SubmitField(label= 'Submit Field')
