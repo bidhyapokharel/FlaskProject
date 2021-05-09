967dc71f080ee108eac6d9550999fb8196d700e1
Bidhya Pokharel <bidhyapokharel1@gmail.com>
2021-05-09 17:34:51 +0545

    Create rEGISTRATION pAGE

---
+++ FlaskProject/market/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+
+class RegistrationForm(FlaskForm):
+    username = StringField(label='username')
+    email_address = StringField(label='email_address')
+    password1 = PasswordField(label='Password')
+    password2 = PasswordField(label='Confirm Password')
+    submit = SubmitField(label= 'Submit Field')
+
