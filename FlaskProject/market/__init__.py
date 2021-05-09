44dcd521f3ea34e3eca432e7b702a2e8011b74d5
Bidhya Pokharel <bidhyapokharel1@gmail.com>
2021-05-09 17:37:03 +0545

    Add ssecret Key to maintain secrecy

--- FlaskProject/market/__init__.py
+++ FlaskProject/market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '22ab2d75d7ed9d73f7f38b7a'
 db = SQLAlchemy(app)
 
 from market import routes
