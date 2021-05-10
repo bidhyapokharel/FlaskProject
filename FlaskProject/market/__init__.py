84568241d76f2ec09c18dcc7e8fdad42c6179857
Bidhya Pokharel <bidhyapokharel1@gmail.com>
2021-05-10 23:00:59 +0545

    Hash the password using flask_bcrypt

--- FlaskProject/market/__init__.py
+++ FlaskProject/market/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '22ab2d75d7ed9d73f7f38b7a'
 db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
 
 from market import routes
 
