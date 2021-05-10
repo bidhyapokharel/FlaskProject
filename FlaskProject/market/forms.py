015b9341f9ef541ce1da60e365bad1b9b7b7bccb
Bidhya Pokharel <bidhyapokharel1@gmail.com>
2021-05-10 20:19:32 +0545

    Fix the email address validation error

--- FlaskProject/market/forms.py
+++ FlaskProject/market/forms.py
@@ -5,12 +5,14 @@ from market.models import User
 
 class RegisterForm(FlaskForm):
     def validate_username(self, username_to_check):
-        user = User.query.filter_by(username=username_to_check).first()
+        user = User.query.filter_by(username=username_to_check.data).first()
         if user:
             raise ValidationError('Username already exists. Please try again later.')
 
-   
-
+    def validate_email_address(self, email_address_to_check):
+        email = User.query.filter_by(email_address=email_address_to_check.data).first()
+        if email:
+            raise ValidationError('Email already exists. Please try again later.')
 
     username = StringField(label='Username: ',validators=[Length(min=2,max=30),DataRequired()])
     email_address = StringField(label='Email Address: ', validators=[Email(), DataRequired()])
