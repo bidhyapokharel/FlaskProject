108d6935165e20189bf37b17323075a90e208248
Bidhya Pokharel <bidhyapokharel1@gmail.com>
2021-05-15 11:30:51 +0545

    Add Functionality for Item Selling

--- FlaskProject/market/__init__.py
+++ FlaskProject/market/__init__.py
@@ -1,19 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
-app.config['SECRET_KEY'] = '22ab2d75d7ed9d73f7f38b7a'
+app.config['SECRET_KEY'] = 'ec9439cfc6c796ae2029594d'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
-login_manager.login_view = 'login_page'
-login_manager.login_message_category = 'info' 
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
 from market import routes
-
-
-# if __name__ == "__main__":
-#     app.run(debug = True)
